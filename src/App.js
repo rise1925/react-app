cd12510739e3a79ca38d9d4047c67be770d38605
react-app <rise1925@naver.com>
2020-10-15 17:11:03 +0900

    hihi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo"/>
+      <h1>let's develop react</h1>
+    </div>
+  );
+}
+
+export default App;
